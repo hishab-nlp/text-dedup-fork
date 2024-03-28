7d413c5037353f8bab17bb867ec8ea0055f8d045
sagor.sarker <sagor.sarker@hishab.co>
2024-03-28 11:50:57 +0000

    update arrow to jsonl for multiprocessing

--- arrow_to_jsonl.py
+++ arrow_to_jsonl.py
@@ -2,23 +2,26 @@ import os
 import json
 from tqdm import tqdm
 from datasets import load_dataset
+import multiprocessing
 
-# Specify the path to your Arrow file
-data_fir = "/data2/deduplication/dedup_output/query_url_tld_bd_cc_2017_2023/arrow_files"
-cache_dir = "/data2/deduplication/cached"
-output_base_path = "/data2/deduplication/dedup_output/query_url_tld_bd_cc_2017_2023/jsonl_files"
+data_dir = "/data2/output/arrows"
+cache_dir = "/data2/cached"
+output_base_path = "/data2/output/cc_culturax"
 os.makedirs(output_base_path, exist_ok=True)
 
-num_shards = 20
-# Load the dataset from the Arrow file
-dataset = load_dataset('arrow', data_dir=data_fir, cache_dir=cache_dir)
+num_shards = 5000
+dataset = load_dataset('arrow', data_dir=data_dir, cache_dir=cache_dir)
 
-# Splitting the dataset into 10 shards
-shards = [dataset['train'].shard(num_shards=num_shards, index=i) for i in range(num_shards)]
-
-for i, shard in tqdm(enumerate(shards), total=len(shards)):
-    output_file = os.path.join(output_base_path, f"chunk_{i}.jsonl")
+def process_shard(index):
+    shard = dataset['train'].shard(num_shards=num_shards, index=index)
+    output_file = os.path.join(output_base_path, f"chunk_{index}.jsonl")
     with open(output_file, 'w') as f:
-        for data in shard:
+        for data in tqdm(shard, total=len(shard)):
+            meta = data['meta']
             json.dump(data, f, ensure_ascii=False)
             f.write('\n')
+
+if __name__ == "__main__":
+    # Using Pool to manage multiple processes
+    with multiprocessing.Pool(processes=multiprocessing.cpu_count()) as pool:
+        list(tqdm(pool.imap(process_shard, range(num_shards)), total=num_shards))
